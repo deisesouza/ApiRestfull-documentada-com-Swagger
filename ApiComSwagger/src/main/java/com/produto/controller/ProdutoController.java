54044000b06074df64917e494c90c09b63646141
Deise Souza <deise_santana_souza@hotmail.com>
2019-04-23 08:22:41 -0300

    Update ProdutoController.java

--- ApiComSwagger/src/main/java/com/produto/controller/ProdutoController.java
+++ ApiComSwagger/src/main/java/com/produto/controller/ProdutoController.java
@@ -3,8 +3,6 @@ package com.produto.controller;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,10 +30,10 @@ ProdutoRepository produtoRepository;
 //ENDPOINTS//
 
 @PostMapping("/produto")
-@ApiOperation(value = "Insere um produto.")
-public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto produto) {	
-	Produto cadastrado = produtoRepository.save(produto);
-	return new ResponseEntity<>(cadastrado, HttpStatus.CREATED);
+@ApiOperation(value = "Salva um produto.")
+public Produto cadastrarProduto(@RequestBody Produto produto) {	
+	return produtoRepository.save(produto);
+	
   }
 
 @DeleteMapping("/produto")
@@ -62,7 +60,6 @@ public Produto alterarProduto(@RequestBody Produto produto) {
 	produtoRepository.save(produto);	
 	return produto;
   }
-
 
 }
 
